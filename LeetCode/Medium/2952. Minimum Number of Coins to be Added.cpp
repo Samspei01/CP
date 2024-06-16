bb9613f19aab4529dfcfa0a7d2279a14b0975b4d
Abdelrhman Saeed elsayd <155198764+Samspei01@users.noreply.github.com>
2024-06-16 10:10:39 +0300

    Create 2952. Minimum Number of Coins to be Added.cpp

---
+++ LeetCode/Medium/2952. Minimum Number of Coins to be Added.cpp
@@ -0,0 +1,22 @@
+// Link: https://leetcode.com/problems/minimum-number-of-coins-to-be-added/
+class Solution {
+public:
+    int minimumAddedCoins(vector<int>& nums, int n) {
+        long long sum=0;
+        int ans=0;
+        sort(nums.begin(),nums.end());
+        for(auto it:nums){
+            while(sum+1<it&&sum<n)
+            {
+
+            sum+=sum+1;
+            ans++;
+            }
+            sum+=it;
+            if(sum>=n)return ans;
+        }
+    
+    while(sum<n)sum+=sum+1,ans++;
+    return ans;
+    }
+};
