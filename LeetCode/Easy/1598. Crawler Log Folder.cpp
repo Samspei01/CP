de1a8bb1f9e511b091474647b121a538d682e5f2
Abdelrhman Saeed Elsayd <155198764+Samspei01@users.noreply.github.com>
2024-07-10 21:42:29 +0300

    Create 1598. Crawler Log Folder.cpp

---
+++ LeetCode/Easy/1598. Crawler Log Folder.cpp
@@ -0,0 +1,19 @@
+// Link: https://leetcode.com/problems/crawler-log-folder/
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        vector<string> paths_stack;
+
+        for (const string& log : logs) {
+            if (log == "../") {
+                if (!paths_stack.empty()) {
+                    paths_stack.pop_back();
+                }
+            } else if (log != "./") {
+                paths_stack.push_back(log);
+            }
+        }
+
+        return paths_stack.size();
+    }
+};
